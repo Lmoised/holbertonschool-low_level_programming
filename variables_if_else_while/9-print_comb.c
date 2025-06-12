ca0d38a2a9c5184b8a8afc2ab96276b4cb49f9d8
vdrdzsilva <vdrdzsilva@penguin>
2025-06-12 18:38:55 -0400

    9 print comb

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 
 /**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int a;
+
+	a = 48;
+	while  (a < 58)
+	{
+		putchar(a);
+		if (a !=  57)
+		{
+			putchar(44);
+			putchar(32);
+			a++;
+		}
+		else
+		{
+			a++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}#include <stdio.h>
+
+/**
  * main - Prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0.
